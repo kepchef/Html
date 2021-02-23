9bbd7aee309a7ed096fee9504dcfbdadb607cc92
kepchef@naver.com <kepchef@naver.com>
2021-02-23 15:36:53 +0900

    2021/02/23 Html 수업예제 등록

---
+++ Kmarket/admin/js/gnb.js
@@ -0,0 +1,13 @@
+$(function(){
+    var gnb = $('.gnb > li > a');
+    gnb.click(function(e){
+        e.preventDefault();
+        var isOpen = $(this).next().is(':visible');
+
+        if(isOpen){
+            $(this).next().slideUp(200);
+        }else{
+            $(this).next().slideDown(200);
+        }
+    });
+});
